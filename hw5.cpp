d354f2573d58e579c2b5112c371718ad21365102
Ben Bryant <brb182@txstate.edu>
2020-10-06 01:25:10 +0000

    fix

---
+++ hw5.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <vector>
+#include "hw5.h"
+
+void BubbleSort(std::vector<std::string> &vec)
+{
+  bool swapped = true;
+  for(int j = 0; j < vec.size()-1; j++)
+    for(int i = 0; i < vec.size()-1; i++)
+      if(vec[i].compare(vec[i+1]) > 0)
+        swap(vec[i], vec[i+1]);
+}
+
+void InsertionSort(std::vector<int> &v)
+{
+  InsertHelper(v, v.size());
+}
+
+void InsertHelper(std::vector<int> &vec, int n)
+{
+  if(n <= 1)
+    return;
+
+  InsertHelper(vec, n-1);
+
+  int last = vec[n-1];
+  int j = n-2;
+
+  while(j >= 0 && vec[j] > last)
+  {
+    vec[j+1] = vec[j];
+    j--;
+  }
+  vec[j+1] = last;
+}
+
+void MergeSort(std::vector<std::string> &vec)
+{
+  int l = 0;
+  int r = vec.size();
+
+  if (l < r)
+  { 
+    int m = l + (r - l) / 2; 
+  
+    MergeSort(vec, l, m); 
+    MergeSort(vec, m + 1, r); 
+  
+    merge(arr, l, m, r); 
+  } 
+}
+
+void MergeHelper(std::vector<std::string> &v, int l, int m, int r)
+{
+  int n1 = m - l + 1; 
+  int n2 = r - m; 
+  std::string L[n1], R[n2]; 
+  
+  for (int i = 0; i < n1; i++) 
+    L[i] = v[l + i]; 
+  for (int j = 0; j < n2; j++) 
+    R[j] = v[m + 1 + j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+  int i = 0; // Initial index of first subarray 
+  int j = 0; // Initial index of second subarray 
+  int k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) { 
+        if (L[i] <= R[j]) { 
+            v[k] = L[i]; 
+            i++; 
+        } 
+        else { 
+            v[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) { 
+        v[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) { 
+        v[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+}
--- hw5.h
+++ hw5.h
@@ -6,10 +6,11 @@ void BubbleSort(std::vector<std::string> &vec);
 // Hint: given two strings str1 and str2, we can use str1.compare(str2). compare() returns 0 if the strings are equal,
 // < 0 if str1 comes first alphabetically, and > 0 if str2 comes first alphabetically.
 
-void InsertionSort(std::vector<std::string> &vec);
+void InsertionSort(std::vector<int>& v);
 // For this problem, re-implement the Insertion Sort algorithm recursively.
 // Rather than use a loop to go through each position we must insert into, use a recursive call. We want to insert the smallest element into the first position first and continue until we insert the largest element into the last position last. Remember that the last recursive call (the base case) finishes executing first and that the first recursive call finishes executing last.
 
+void InsertHelper(std::vector<int> &vec, int n);
 
 void MergeSort(std::vector<std::string> &vec);
 // Complete the functions to implement MergeSort on a vector of strings to sort it in alphabetical order.
@@ -18,7 +19,6 @@ void MergeSort(std::vector<std::string> &vec);
 // last element of the right-hand sub-vector.
 // Hint: MergeSort (with a single parameter, the vector to be sorted) makes a single call to its helper function.
 
-
-
+void MergeHelper(std::vector<std::string> &v, int l, int m, int r);
 
 #endif /* __HW5_H__ */
